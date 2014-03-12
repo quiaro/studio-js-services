3409f1199b6ce67af61b7424adddaaf66658a7c7
quiaro <david.quiros@rivetlogic.com>
2014-03-12 16:29:31 -0600

    Makes utils module return a factory

--- src/utils.js
+++ src/utils.js
@@ -1,76 +1,74 @@
 /* global define, DEBUG */
 
-define(['config'], function(CFG) {
+define(['request_agent', 'config'], function(requestAgent, CFG) {
 
     'use strict';
 
-    var API = {};
+    var module = function (customConfig) {
+        this.config = requestAgent.extend(true, {}, CFG, customConfig);
 
-    function setSite(siteName) {
+        if (DEBUG) {
+            console.log('Studio Services configuration: ', this.config);
+        }
+    };
 
+    module.prototype.getBaseUrl = function getBaseUrl() {
+        var path;
+
+        if (this.config.services.domain) {
+            path = [this.config.services.protocol, '://',
+                    this.config.services.domain, ':', this.config.services.port,
+                    '/', this.config.api.base, '/', this.config.api.version].join('');
+        } else {
+            path = ['/', this.config.api.base, '/', this.config.api.version].join('');
+        }
+        return path;
+    };
+
+    module.prototype.getSite = function getSite() {
+        return this.config.services.site;
+    };
+
+    module.prototype.setSite = function setSite(siteName) {
         if (typeof siteName === 'string' && !!siteName) {
-            CFG.services.site = siteName;
-            return CFG.services.site;
+            this.config.services.site = siteName;
+            return this.config.services.site;
         } else {
             throw new Error('Incorrect value for site name');
         }
-    }
-
-    function getSite() {
-        return CFG.services.site;
-    }
-
-    function getBaseUrl() {
-        var path;
-
-        if (CFG.services.domain) {
-            path = [CFG.services.protocol, '://',
-                    CFG.services.domain, ':', CFG.services.port,
-                    '/', CFG.api.base, '/', CFG.api.version].join('');
-        } else {
-            path = ['/', CFG.api.base, '/', CFG.api.version].join('');
-        }
-        return path;
-    }
-
-    function logService(service) {
-        console.log(service.name + ' | base URL: ', service.url);
-    }
-
-    /*
-     * @param method : an object with the method properties (: name, arguments, url, promise)
-     */
-    function logMethod(method) {
-        console.log('--------------------------------');
-        console.log('Calling ' + method.name + ' ...');
-        console.log('Params: ', method.params);
-
-        if (method.promise) {
-            method.promise.done(function(result) {
-                console.log('*** Request from ' + method.name);
-                console.log('*** URL: ' + method.url);
-                console.log('*** RESOLVED: ', result);
-            });
-            method.promise.fail(function(reason){
-                console.log('*** Request from ' + method.name);
-                console.log('*** URL: ' + method.url);
-                console.log('*** FAILED: ', reason);
-            });
-        } else {
-            console.log('*** No promise for: ' + method.name);
-        }
-    }
-
-    API.getSite = getSite;
-    API.getBaseUrl = getBaseUrl;
-    API.setSite = setSite;
+    };
 
     if (DEBUG) {
-        // Expose debug methods
-        API.logMethod = logMethod;
-        API.logService = logService;
+        module.prototype.logService = function logService(service) {
+            console.log(service.name + ' | base URL: ', service.url);
+        };
+
+        /*
+         * @param method : an object with the method properties (: name, arguments, url, promise)
+         */
+        module.prototype.logMethod = function logMethod(method) {
+            console.log('--------------------------------');
+            console.log('Calling ' + method.name + ' ...');
+            console.log('Params: ', method.params);
+
+            if (method.promise) {
+                method.promise.done(function(result) {
+                    console.log('*** Request from ' + method.name);
+                    console.log('*** URL: ' + method.url);
+                    console.log('*** RESOLVED: ', result);
+                });
+                method.promise.fail(function(reason){
+                    console.log('*** Request from ' + method.name);
+                    console.log('*** URL: ' + method.url);
+                    console.log('*** FAILED: ', reason);
+                });
+            } else {
+                console.log('*** No promise for: ' + method.name);
+            }
+        };
     }
 
-    return API;
+    return module;
+
 });
 
